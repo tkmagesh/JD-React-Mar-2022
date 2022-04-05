e33e1585f3c14a28ef56560e69a77c7d1e52bb8a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-05 09:48:33 +0530

    Refactoring Spinner - 2. Remove the dependency on the store from the component

---
+++ 06-state-manager-refactored/state-manager.js
@@ -0,0 +1,37 @@
+let StateManager = (function(){
+    let _currentState = undefined,
+        _subscribers = [],
+        _reducer = undefined,
+        _init_action = { type : '@@INIT/ACTION' }
+
+    function getState(){
+        return _currentState;
+    }
+
+    function subscribe(callback){
+        _subscribers.push(callback);
+    }
+
+    //private 
+    function emitChange(){
+        _subscribers.forEach(callback => callback());
+    }
+
+    function dispatch(action){
+        let newState = _reducer(_currentState, action);
+        if (newState === _currentState) return; //NO state changes
+        _currentState = newState;
+        emitChange()
+    }
+
+    function createStore(reducer){
+        if (typeof reducer !== 'function')
+            throw new Error("reducer is mandatory to create the store");
+        _reducer = reducer;
+        _currentState = _reducer(undefined, _init_action) // TO INITIALIZE THE CURRENT STATE with a VALID DEFAULT STATE
+        let store = { getState, subscribe, dispatch }
+        return store;
+    }
+
+    return { createStore };    
+})();
