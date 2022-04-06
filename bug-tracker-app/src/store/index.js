b55517dc146f6455936c7bc446104503267010d5
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-06 15:28:08 +0530

    middlewares

--- bug-tracker-app/src/store/index.js
+++ bug-tracker-app/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import bugsReducer from '../bugs/reducers/bugsReducer'
 import projectsReducer from '../projects/reducers/projectsReducer';
 
@@ -7,6 +7,28 @@ const rootReducer = combineReducers({
     bugs : bugsReducer
 });
 
-const store = createStore(rootReducer);
+/* function loggerMiddleware(store){
+    return function(next){
+        return function(action){
+            console.group(action.type);
+            console.log('before - ', store.getState())
+            console.log(action)
+            next(action)
+            console.log('after - ', store.getState())
+            console.groupEnd()
+        }
+    }
+} */
+
+const loggerMiddleware = store => next => action => {
+    console.group(action.type);
+    console.log('before - ', store.getState())
+    console.log(action)
+    next(action)
+    console.log('after - ', store.getState())
+    console.groupEnd()
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default store;
